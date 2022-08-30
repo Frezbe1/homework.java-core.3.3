024c72f4342dabfeb3c59e951e6bbc80f9144ede
Шалагинов Виталий <v.a.sh@mail.ru>
2022-08-31 02:51:33 +0700

    java-core 3.3

---
+++ Main.java
@@ -0,0 +1,43 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class Main {
+    public static void main(String[] args) {
+        openZip("C://Games//savesgames//zip.zip");
+        openProgress("C://Games//savesgames//save1.dat");
+    }
+
+    public static void openZip(String urlFile) {
+        try (ZipInputStream zin = new ZipInputStream(new FileInputStream(urlFile))) {
+            ZipEntry entry;
+            String name;
+            while ((entry = zin.getNextEntry()) != null) {
+                name = entry.getName();
+                FileOutputStream fout = new FileOutputStream(name);
+                for (int c = zin.read(); c != -1; c = zin.read()) {
+                    fout.write(c);
+                }
+                fout.flush();
+                zin.closeEntry();
+                fout.close();
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+
+    public static GameProgress openProgress(String urlSave) {
+        GameProgress gameProgress = null;
+        try (FileInputStream fis = new FileInputStream(urlSave);
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
+            gameProgress = (GameProgress) ois.readObject();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return gameProgress;
+    }
+}
